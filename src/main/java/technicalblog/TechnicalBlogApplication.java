38828f79453a326ea20c9058f06d02166fc3bffe
Amanpreet Singh <amansi@microsoft.com>
2019-11-01 11:32:49 +0530

    First Milestone

---
+++ src/main/java/technicalblog/TechnicalBlogApplication.java
@@ -0,0 +1,11 @@
+package technicalblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalBlogApplication {
+    public static void main(String[] Args){
+        SpringApplication.run(TechnicalBlogApplication.class, Args);
+    }
+}
